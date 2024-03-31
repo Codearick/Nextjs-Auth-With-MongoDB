92508efc5f6c30cde8bbcecb28bf05e231b6c397
Codearick <rohan.gautam650@gmail.com>
2024-04-01 04:36:13 +0530

    add middleware of the app

---
+++ src/middleware.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+    const path = request.nextUrl.pathname;
+
+    const publicPath = path === '/login' || path === '/signup' || path === '/verifyemail';
+
+    const token = request.cookies.get("token")?.value || ""
+
+    if(publicPath && token){
+        return NextResponse.redirect(new URL('/profile', request.url))
+    }
+
+    if(!publicPath && !token){
+        return NextResponse.redirect(new URL('/login', request.url))
+    }
+
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    '/profile/:path*',
+    '/login',
+    '/signup',
+    '/verifyemail',
+    '/'
+]
+}
